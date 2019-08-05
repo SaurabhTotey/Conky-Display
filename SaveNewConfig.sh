863e31c304602fd3ddfcb9529e606e78df5ee7cd
Saurabh Totey <SaurabhTotey@gmail.com>
2019-08-05 17:09:22 -0600

    Initial project

---
+++ SaveNewConfig.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Moves the currently in-use config to the old-version folder
+cp ~/.config/.conkyrc ./old-version/.conkyrc -f
+
+#Commits the now-moved config into version history so that it can be referenced later if necessary
+cd ./old-version || exit
+versionNumber=$(git rev-list --all --count)
+git add -A > /dev/null 2>&1 &
+git commit -m "$versionNumber" > /dev/null 2>&1 &
+cd ..
+
+#Moves the new config to the location where conky will look for it and use it, replacing the old one
+cp ./.conkyrc ~/.config/.conkyrc -f
+
+#Restarts Conky
+pkill -f conky
+conky -b > /dev/null 2>&1 &
