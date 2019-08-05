8cd85f49200ba57155b74cd0cadcad0f1651ae4a
Saurabh Totey <SaurabhTotey@gmail.com>
2019-08-05 17:31:45 -0600

    Fixed .conkyrc path and updated config

--- SaveNewConfig.sh
+++ SaveNewConfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Moves the currently in-use config to the old-version folder
-cp ~/.config/.conkyrc ./old-version/.conkyrc -f
+cp ~/.conkyrc ./old-version/.conkyrc -f
 
 #Commits the now-moved config into version history so that it can be referenced later if necessary
 cd ./old-version || exit
@@ -11,7 +11,7 @@ git commit -m "$versionNumber" > /dev/null 2>&1 &
 cd ..
 
 #Moves the new config to the location where conky will look for it and use it, replacing the old one
-cp ./.conkyrc ~/.config/.conkyrc -f
+cp ./.conkyrc ~/.conkyrc -f
 
 #Restarts Conky
 pkill -f conky
