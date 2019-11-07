9362061e8891e0d33bd44c56ae7c7003129c8770
SaurabhTotey <SaurabhTotey@gmail.com>
2019-11-07 10:00:25 -0700

    Gave NetworkUtility an update function

--- src/NetworkUtility.lua
+++ src/NetworkUtility.lua
@@ -5,19 +5,32 @@ local NetworkUtility = {}
 --How often the network utility will actually send out new requests in seconds
 NetworkUtility.refreshRate = 30
 
+--A table that maps a path string to an object; the object is a pair containing requestTime and the request's data
 NetworkUtility.cache = {}
+
+--[[
+Should be called periodically to update the network utility
+Cleans the cache of old data
+]]
+function NetworkUtility.update()
+	for path, data in NetworkUtility.cache do
+		if os.time() - NetworkUtility.refreshRate > data.requestTime then
+			NetworkUtility.cache[path] = nil
+		end
+	end
+end
 
 --[[
 Gets the data at the given path
 If the data was requested recently enough, a cached version of the data is given instead
+Relies on the update function being called regularly to clear the cache of old data
 ]]
 function NetworkUtility.request(path)
 	local cached = NetworkUtility.cache[path]
-	local currentTime = os.time()
-	if cached == nil || currentTime - NetworkUtility.refreshRate > cached.requestTime then
+	if cached == nil then
 		local requestData, status, headers = http.request(path)
 		NetworkUtility.cache[path] = {}
-		NetworkUtility.cache[path].requestTime = currentTime
+		NetworkUtility.cache[path].requestTime = os.time()
 		NetworkUtility.cache[path].data = requestData
 		return requestData
 	else
