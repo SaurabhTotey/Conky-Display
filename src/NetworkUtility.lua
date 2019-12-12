ecd1727aa75c19e6986070329e26d96754082913
SaurabhTotey <SaurabhTotey@gmail.com>
2019-12-12 13:41:53 -0700

    Removed extraneous data stored in NetworkUtility

--- src/NetworkUtility.lua
+++ src/NetworkUtility.lua
@@ -2,7 +2,7 @@ local http = require("socket.http")
 
 local NetworkUtility = {}
 
---A table that maps a path string to an object; the object is a triple containing requestTime, expirationDuration, and the request's data
+--A table that maps a path string to an object; the object is a pair containing expirationTime, and the request's data
 NetworkUtility.cache = {}
 
 --[[
@@ -18,7 +18,7 @@ Cleans the cache of old data
 ]]
 function NetworkUtility.update()
 	for path, data in pairs(NetworkUtility.cache) do
-		if os.time() - data.expirationDuration > data.requestTime then
+		if os.time() >= data.expirationTime then
 			NetworkUtility.cache[path] = nil
 		end
 	end
@@ -29,22 +29,24 @@ Gets the data at the given path
 If the data was retrieved recently enough, a cached version of the data is given instead
 Relies on the update function being called regularly to clear the cache of old data
 expirationDuration is an optional parameter that specifies how long the data should live in the cache in seconds (defaults to 30)
-If the same request is made with a new and different expirationDuration, the request is sent out again and refreshed in the cache
+If the same request is made with a new and different expirationDuration, the request will obey the previous expirationDuration until removed from the cache
+forceRefresh is an optional paramter that specifies whether to refresh the cache for this specific request (defaults to false)
 ]]
-function NetworkUtility.request(path, expirationDuration)
+function NetworkUtility.request(path, expirationDuration, forceRefresh)
 
 	--Getting parameters
 	expirationDuration = expirationDuration or 30;
+	forceRefresh = forceRefresh or false;
 
-	--Get cached data if possible
+	--Get cached data if it exists and not forcing HTTP
 	local cached = NetworkUtility.cache[path]
-	if cached ~= nil and cached.expirationDuration == expirationDuration then
+	if cached ~= nil and not forceRefresh then
 		return cached.data
 	end
 
 	--Actually send out request and update cache with request data
 	local requestData, status, headers = http.request(path)
-	NetworkUtility.cache[path] = { requestTime = os.time(), expirationDuration = expirationDuration, data = requestData }
+	NetworkUtility.cache[path] = { expirationTime = os.time() + expirationDuration, data = requestData }
 	return requestData
 end
 
