d4b275452b1a7e4c16fab552a0c1489dfb61219a
SaurabhTotey <SaurabhTotey@gmail.com>
2019-12-12 13:33:27 -0700

    Allowed requests to specify their own expiration duration

--- src/NetworkUtility.lua
+++ src/NetworkUtility.lua
@@ -1,12 +1,8 @@
 local http = require("socket.http")
 
---TODO: allow each request to specify its own expiration duration
 local NetworkUtility = {}
 
---How much time must pass before request data will be fetched over the network rather than being retrieved from a cached value in seconds
-NetworkUtility.expirationDuration = 30
-
---A table that maps a path string to an object; the object is a pair containing requestTime and the request's data
+--A table that maps a path string to an object; the object is a triple containing requestTime, expirationDuration, and the request's data
 NetworkUtility.cache = {}
 
 --[[
@@ -22,7 +18,7 @@ Cleans the cache of old data
 ]]
 function NetworkUtility.update()
 	for path, data in pairs(NetworkUtility.cache) do
-		if os.time() - NetworkUtility.expirationDuration > data.requestTime then
+		if os.time() - data.expirationDuration > data.requestTime then
 			NetworkUtility.cache[path] = nil
 		end
 	end
@@ -32,14 +28,23 @@ end
 Gets the data at the given path
 If the data was retrieved recently enough, a cached version of the data is given instead
 Relies on the update function being called regularly to clear the cache of old data
+expirationDuration is an optional parameter that specifies how long the data should live in the cache in seconds (defaults to 30)
+If the same request is made with a new and different expirationDuration, the request is sent out again and refreshed in the cache
 ]]
-function NetworkUtility.request(path)
+function NetworkUtility.request(path, expirationDuration)
+
+	--Getting parameters
+	expirationDuration = expirationDuration or 30;
+
+	--Get cached data if possible
 	local cached = NetworkUtility.cache[path]
-	if cached ~= nil then
+	if cached ~= nil and cached.expirationDuration == expirationDuration then
 		return cached.data
 	end
+
+	--Actually send out request and update cache with request data
 	local requestData, status, headers = http.request(path)
-	NetworkUtility.cache[path] = { requestTime = os.time(), data = requestData }
+	NetworkUtility.cache[path] = { requestTime = os.time(), expirationDuration = expirationDuration, data = requestData }
 	return requestData
 end
 
